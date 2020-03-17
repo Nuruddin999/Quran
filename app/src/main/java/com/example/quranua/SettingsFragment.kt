91a69d667f7fe597932e7fb433e48ace3f769f4d
Nuruddin <sg7720@gmail.com>
2020-03-17 11:30:23 +0300

    settings menu

--- app/src/main/java/com/example/quranua/SettingsFragment.kt
+++ app/src/main/java/com/example/quranua/SettingsFragment.kt
@@ -1,12 +1,15 @@
 package com.example.quranua
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.quranua.MainList.MainListAdapter
 import com.example.quranua.Model.Verse
+import kotlinx.android.synthetic.main.settings_fragment_layout.*
 
 class SettingsFragment:BaseFragment() {
     override fun onCreateView(
@@ -15,7 +18,8 @@ class SettingsFragment:BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.settings_fragment_layout, container, false)
-
+        var seekbar:SeekBar=view.findViewById(R.id.settings_textsizeseekbar)
+seekbar.progressDrawable.setColorFilter(resources.getColor(R.color.colorPrimary),android.graphics.PorterDuff.Mode.SRC_IN)
         return view
 
     }
